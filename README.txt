297c619da1f1b48ba04195347de5dc22a0bc1493
VuYeK <vuyek04@gmail.com>
2016-08-25 16:50:04 +0200

    + readme + empty controller

---
+++ README.txt
@@ -0,0 +1,1 @@
+Zadanie rekrutacyjne z PHP - Obsługa plików IGC zawierających przebieg lotu paralotniowego.
---
+++ igcportal/controller/controller.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VuYeK
+ * Date: 25.08.2016
+ * Time: 16:49
+ */
