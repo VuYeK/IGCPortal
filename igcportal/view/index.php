00a0fffbedc45352f9ea6cf74ea5f61be7bf726e
VuYeK <vuyek04@gmail.com>
2016-08-25 16:41:41 +0200

    + template

---
+++ igcportal/view/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+
+    <title>IGCPortal</title>
+    <meta name="description" content="Obsługa plików IGC">
+    <meta name="author" content="VuYeK">
+    <meta name="keywords" content="igc, paragliding"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+
+    <link rel="shortcut icon" href="favicon.png"/>
+
+    <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!--<link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>-->
+</head>
+<body>
+IGCPortal
+</body>
+</html>
